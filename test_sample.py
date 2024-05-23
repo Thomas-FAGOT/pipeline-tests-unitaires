2b74c23ac8a98b161f50db2448be027600c535bb
Thomas-FAGOT <thomas.fagot@ynov.com>
2024-05-23 15:08:18 +0200

    add test

---
+++ test_sample.py
@@ -0,0 +1,15 @@
+def func(x):
+    return x + 1
+
+
+def test_answer():
+    assert func(3) == 4
+
+def test_fonc():
+    assert func(4) == 5
+
+def test_fonc2():
+    assert func(5) == 6
+
+def test_fonc3():
+    assert func(5) == 8
